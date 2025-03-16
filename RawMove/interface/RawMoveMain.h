e3569dcf3ae7bc4e06dcbd570859f16ccd7ee2e9
Yuluo00 <2646310425@qq.com>
2025-03-17 00:37:30 +0800

    【添加项目】【RawMove】 基础的空DLL项目

---
+++ RawMove/interface/RawMoveMain.h
@@ -0,0 +1,11 @@
+﻿#ifndef RAW_MOVE_MAIN_H
+#define RAW_MOVE_MAIN_H
+
+
+ZYB_RAWMOVE_API void say_hello();
+
+
+#endif // !RAW_MOVE_MAIN_H
+
+
+
---
+++ RawMove/src/RawMoveMain.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+#include <Windows.h>
+
+
+ZYB_RAWMOVE_API void say_hello()
+{
+    std::cout << "Hello from MyLibrary!" << std::endl;
+}
+
+// DLL 加载时调用
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        //std::cout << "Library Loaded!" << std::endl;
+        break;
+    case DLL_PROCESS_DETACH:
+        //std::cout << "Library Unloaded!" << std::endl;
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
