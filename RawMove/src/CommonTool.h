c3c14d16a41f4e974b2888560994313480a620bc
Yuluo00 <2646310425@qq.com>
2025-03-17 21:45:49 +0800

    【RawMove】添加-导入 CommonTool.h(cpp)

---
+++ RawMove/src/CommonTool.h
@@ -0,0 +1,20 @@
+﻿#include <Windows.h>
+
+#include <string>
+#include <vector>
+
+namespace CommonTool {
+    std::string Local2Utf8(const std::string &str);
+    std::string Utf82Local(const std::string &str);
+
+    std::string Wstr2Utf8(const std::wstring &wstr);
+    std::string Wstr2Local(const std::wstring &wstr);
+
+    std::wstring Utf82Wstr(const std::string &str);
+    std::wstring Local2Wstr(const std::string &str);
+
+    bool StrIsUtf8Bom(const std::string &str);
+    bool FileIsUtf8Bom(const std::string &filePath);
+
+    std::vector<std::string> ReadFileTxtAsLocal(const std::string &filePath);
+}
