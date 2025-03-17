9969d4b9376b989e5b3c7ac705f6a2ffb2aa40c7
Yuluo00 <2646310425@qq.com>
2025-03-17 21:28:27 +0800

    【RawMoveUI】添加对CLR的引用和测试

--- RawMoveUI/Program.cs
+++ RawMoveUI/Program.cs
@@ -1,3 +1,5 @@
+
+
 namespace RawMoveUI
 {
     internal static class Program
@@ -8,6 +10,7 @@ namespace RawMoveUI
         [STAThread]
         static void Main()
         {
+            NS_RawMoveCLR.RawMoveCLR.test();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
