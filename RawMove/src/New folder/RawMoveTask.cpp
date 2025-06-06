c6b4ac205d728d0509eef8d4d6c507193238310f
yubizhan <yubizhan@qti.qualcomm.com>
2025-06-06 14:55:50 +0800

    1

---
+++ RawMove/src/New folder/RawMoveTask.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yubizhan on 6/6/2025.
+//
+
+#include "RawMoveTask.h"
+
+
+
+RawMoveTask::RawMoveTask() = default;
+RawMoveTask::~RawMoveTask() = default;
---
+++ RawMove/src/New folder/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+#include "GltfRender.h"
+#include  "RawMoveTask.h"
+
+#include  <fmt/xchar.h>
+
+int main()
+{
+
+    std::wstring name = L"世界";
+    fmt::format(L"你好，{}！\n", name);
+
+    RawMoveTask rawMoveTask;
+    return 0;
+}
