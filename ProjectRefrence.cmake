ef36783aac9976dc8db6c0fb724484d69adbd3c5
Yuluo00 <2646310425@qq.com>
2025-06-15 12:58:58 +0800

    【项目配置】Add3rd_libarchive.cmake

--- ProjectRefrence.cmake
+++ ProjectRefrence.cmake
@@ -89,6 +89,23 @@ endmacro()
 
 # -------------------------------------------------------------------------------- 导入三方库 -------------------------------------
 
+
+macro(Add3rd_LibArchive ProjectName)
+    message("LibArchive == ${ProjectRootDir}/ThirdParty/libarchive/installed/x64-windows/")
+    set(CMAKE_INCLUDE_PATH "${ProjectRootDir}/ThirdParty/libarchive/installed/x64-windows/include")
+    set(CMAKE_LIBRARY_PATH "${ProjectRootDir}/ThirdParty/libarchive/installed/x64-windows/lib")
+
+    find_package(LibArchive REQUIRED)
+    target_link_libraries(${ProjectName} PRIVATE LibArchive::LibArchive) # since CMake 3.17
+
+    install(DIRECTORY
+        $<$<CONFIG:Debug>:${ProjectRootDir}/ThirdParty/libarchive/installed/x64-windows/debug/bin/>
+        $<$<CONFIG:Release>:${ProjectRootDir}/ThirdParty/libarchive/installed/x64-windows/bin/>
+        DESTINATION bin
+        FILES_MATCHING PATTERN "*.dll"
+    )
+endmacro()
+
 macro(Add3rd_7zExtra ProjectName)
     list(APPEND ALL_IMPORTED_LOCATION "${ProjectRootDir}/ThirdParty/7z2409-extra/x64/7za.dll")
 endmacro()
