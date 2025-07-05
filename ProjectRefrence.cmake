f945d3a544c1fb8280ea3441852efcc42e0243e2
Yuluo00 <2646310425@qq.com>
2025-07-05 20:27:22 +0800

    【项目配置】【三方库导入接口】spdlog
    # Conflicts:
    #	ProjectRefrence.cmake

--- ProjectRefrence.cmake
+++ ProjectRefrence.cmake
@@ -176,3 +176,15 @@ macro(Add3rd_FFmpeg ProjectName)
         ${ProjectRootDir}/ThirdParty/ffmpeg/installed/x64-windows/bin/swscale-8.dll
     )
 endmacro()
+
+macro(Add3rd_spdlog ProjectName)
+    set(fmt_DIR "${ProjectRootDir}/ThirdParty/spdlog/installed/x64-windows/share/fmt")
+    set(spdlog_DIR "${ProjectRootDir}/ThirdParty/spdlog/installed/x64-windows/share/spdlog")
+    message("spdlog_DIR == ${spdlog_DIR}")
+
+    find_package(spdlog CONFIG REQUIRED)
+    target_link_libraries(${ProjectName} PRIVATE spdlog::spdlog)
+
+    Add_Interface_Imported_Location(spdlog::spdlog)
+endmacro()
+
