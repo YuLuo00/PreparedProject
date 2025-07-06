817fd5da3980192868ee471abacbcec26928551f
Yuluo00 <2646310425@qq.com>
2025-07-06 16:19:09 +0800

    【项目配置】【三方库导入接口】sqlite3
    # Conflicts:
    #	ProjectRefrence.cmake

--- ProjectRefrence.cmake
+++ ProjectRefrence.cmake
@@ -65,3 +65,14 @@ macro(Add3rd_nlohmann_json ProjectName)
     target_link_libraries(${ProjectName} PRIVATE nlohmann_json::nlohmann_json)
     Add_Interface_Imported_Location(nlohmann_json::nlohmann_json)
 endmacro()
+
+macro(Add3rd_sqlite3 ProjectName)
+    set(unofficial-sqlite3_DIR "${ProjectRootDir}/ThirdParty/sqlite3/installed/x64-windows/share/unofficial-sqlite3/")
+    message("unofficial-sqlite3_DIR == ${unofficial-sqlite3_DIR}")
+
+    find_package(unofficial-sqlite3 CONFIG REQUIRED)
+    target_link_libraries(${ProjectName} PRIVATE unofficial::sqlite3::sqlite3)
+    
+    Add_Interface_Imported_Location(unofficial::sqlite3::sqlite3)
+    Add_Imported_Location(unofficial::sqlite3::sqlite3)
+endmacro()
