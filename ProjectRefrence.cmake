496492ab7180205c6887d302da7fc145a86352f6
Yuluo00 <2646310425@qq.com>
2025-10-12 16:29:46 +0800

    【项目配置】【三方库导入接口】opencv
    # Conflicts:
    #	ProjectRefrence.cmake

--- ProjectRefrence.cmake
+++ ProjectRefrence.cmake
@@ -136,3 +136,50 @@ macro(Add3rd_OpenCV ProjectName)
         png
     )
 endmacro()
+
+macro(Add3rd_sqlite3 ProjectName)
+    set(unofficial-sqlite3_DIR "${ProjectRootDir}/ThirdParty/sqlite3/installed/x64-windows/share/unofficial-sqlite3/")
+    message("unofficial-sqlite3_DIR == ${unofficial-sqlite3_DIR}")
+
+    find_package(unofficial-sqlite3 CONFIG REQUIRED)
+    target_link_libraries(${ProjectName} PRIVATE unofficial::sqlite3::sqlite3)
+    
+    Add_Interface_Imported_Location(unofficial::sqlite3::sqlite3)
+    Add_Imported_Location(unofficial::sqlite3::sqlite3)
+endmacro()
+
+macro(Add3rd_OpenCV411 ProjectName)
+    set(Protobuf_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/share/protobuf/")
+    set(quirc_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/share/quirc/")
+    set(OpenCV_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/share/opencv4/")
+    
+    set(TIFF_INCLUDE_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/include/")
+    set(TIFF_LIBRARY "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/bin/")
+    list(APPEND ALL_IMPORTED_LOCATION_Debug "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/debug/bin/tiffd.dll")
+    list(APPEND ALL_IMPORTED_LOCATION_Release "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/bin/tiffd.dll")
+    message("OpenCV_DIR == ${OpenCV_DIR}")
+
+    set(libjpeg-turbo_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/share/libjpeg-turbo/")
+    set(WebP_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/share/WebP/")
+    set(libpng_DIR "${ProjectRootDir}/ThirdParty/opencv411/installed/x64-windows/share/libpng/")
+    find_package(libpng CONFIG REQUIRED)
+    find_package(libjpeg-turbo CONFIG REQUIRED)
+    find_package(WebP CONFIG REQUIRED)
+    # this is heuristically generated, and may not be correct
+    find_package(OpenCV CONFIG REQUIRED)
+    
+
+    # note: 10 additional targets are not displayed.
+    target_link_libraries(${ProjectName} PRIVATE
+        opencv_ml opencv_dnn opencv_core opencv_flann opencv_imgcodecs opencv_imgproc
+        libjpeg-turbo::jpeg
+        WebP::webp WebP::webpdecoder WebP::webpdemux WebP::sharpyuv
+        png
+    )
+    Add_Interface_Imported_Location(
+        opencv_ml opencv_dnn opencv_core opencv_flann opencv_imgcodecs opencv_imgproc
+        libjpeg-turbo::jpeg
+        WebP::webp WebP::webpdecoder WebP::webpdemux WebP::sharpyuv
+        png
+    )
+endmacro()
