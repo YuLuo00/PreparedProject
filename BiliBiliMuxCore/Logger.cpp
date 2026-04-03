cd8862f22899f19dd027bfb6de4b30c00af44129
Yuluo00 <2646310425@qq.com>
2026-04-04 02:19:09 +0800

    Logger

---
+++ BiliBiliMuxCore/Logger.cpp
@@ -0,0 +1,49 @@
+#include "Logger.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+std::unordered_map<std::string, std::shared_ptr<spdlog::logger>> Logger::s_loggers;
+
+static std::vector<spdlog::sink_ptr> g_sinks;
+
+void Logger::Init()
+{
+    if (!g_sinks.empty())
+        return;
+
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        "logs/app.log", 1024 * 1024 * 5, 3);
+
+    g_sinks = { console_sink, file_sink };
+}
+
+std::shared_ptr<spdlog::logger> Logger::Get(const std::string& group)
+{
+    if (g_sinks.empty())
+        Init();
+
+    auto it = s_loggers.find(group);
+    if (it != s_loggers.end())
+        return it->second;
+
+    // 创建新的 group logger
+    auto logger = std::make_shared<spdlog::logger>(group, g_sinks.begin(), g_sinks.end());
+
+    // 格式：yyyy::mm::dd hh::mm::ss [level][group] 内容
+    logger->set_pattern("%Y::%m::%d %H::%M::%S [%^%l%$][%n] %v");
+
+    logger->set_level(spdlog::level::trace); // 默认全开
+    logger->flush_on(spdlog::level::err);
+
+    spdlog::register_logger(logger);
+
+    s_loggers[group] = logger;
+    return logger;
+}
+
+void Logger::SetLevel(const std::string& group, spdlog::level::level_enum level)
+{
+    auto logger = Get(group);
+    logger->set_level(level);
+}
---
+++ BiliBiliMuxCore/Logger.h
@@ -0,0 +1,40 @@
+#pragma once
+
+
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <spdlog/spdlog.h>
+
+class Logger
+{
+public:
+    static void Init();
+
+    // 获取指定 group 的 logger
+    static std::shared_ptr<spdlog::logger> Get(const std::string& group);
+
+    // 设置某个 group 的日志级别
+    static void SetLevel(const std::string& group, spdlog::level::level_enum level);
+
+private:
+    static std::unordered_map<std::string, std::shared_ptr<spdlog::logger>> s_loggers;
+};
+
+namespace LogGroup
+{
+    constexpr const char* MUX = "mux";
+    constexpr const char* DECODE = "decode";
+    constexpr const char* IO = "io";
+}
+
+
+// 初始化
+#define LOG_INIT() Logger::Init()
+// 自动获取 logger
+#define LOG_TRACE(group, ...) Logger::Get(group)->trace(__VA_ARGS__)
+#define LOG_DEBUG(group, ...) Logger::Get(group)->debug(__VA_ARGS__)
+#define LOG_INFO(group, ...)  Logger::Get(group)->info(__VA_ARGS__)
+#define LOG_WARN(group, ...)  Logger::Get(group)->warn(__VA_ARGS__)
+#define LOG_ERROR(group, ...) Logger::Get(group)->error(__VA_ARGS__)
+#define LOG_CRITICAL(group, ...) Logger::Get(group)->critical(__VA_ARGS__)
