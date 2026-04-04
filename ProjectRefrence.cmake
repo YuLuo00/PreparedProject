cc1b1db0e1afd0420f2cc0e815d1eeecef5f231f
Yuluo00 <2646310425@qq.com>
2026-04-05 01:53:43 +0800

    【项目配置】【三方库导入接口】tbb

--- ProjectRefrence.cmake
+++ ProjectRefrence.cmake
@@ -302,3 +302,14 @@ macro(Add3rd_OpenCV411 ProjectName)
         png
     )
 endmacro()
+
+macro(Add3rd_TBB ProjectName)
+    set(TBB_DIR "${ProjectRootDir}/ThirdParty/tbb/installed/x64-windows/share/tbb//")
+    message("TBB_DIR == ${TBB_DIR}")
+
+    find_package(TBB CONFIG REQUIRED)
+    target_link_libraries(${ProjectName} PRIVATE TBB::tbb)
+    
+    Add_Interface_Imported_Location(TBB::tbb)
+    Add_Imported_Location(TBB::tbb)
+endmacro()
