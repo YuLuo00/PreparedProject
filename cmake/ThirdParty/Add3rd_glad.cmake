77cf50bb59e7c14d49d55751effad059b0dc2348
yubizhan <yubizhan@qti.qualcomm.com>
2026-05-28 12:39:37 +0800

    【项目配置】Add3rd_glad.cmake

---
+++ cmake/ThirdParty/Add3rd_glad.cmake
@@ -0,0 +1,12 @@
+# 使用前需设置变量: ProjectName
+# 示例:
+#   set(ProjectName MyTarget)
+#   include(${ProjectRootDir}/cmake/ThirdParty/glad.cmake)
+
+set(glad_DIR "${ProjectRootDir}/ThirdParty/glad/installed/x64-windows/share/glad")
+message("glad_DIR == ${glad_DIR}")
+
+find_package(glad CONFIG REQUIRED)
+target_link_libraries(${ProjectName} PRIVATE glad::glad)
+
+Add_Interface_Imported_Location(glad::glad)
