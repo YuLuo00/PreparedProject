54f85e5cde6bd0c142c2163443ac1b0e02afe529
yubizhan <yubizhan@qti.qualcomm.com>
2026-05-28 12:21:49 +0800

    fixup! 【项目配置】Add3rd_7zextra.cmake

--- cmake/ThirdParty/Add3rd_7zextra.cmake
+++ cmake/ThirdParty/Add3rd_7zextra.cmake
@@ -1,6 +1,1 @@
-# 使用前需设置变量: ProjectName
-# 示例:
-#   set(ProjectName MyTarget)
-#   include(${ProjectRootDir}/cmake/ThirdParty/7zextra.cmake)
-
-list(APPEND ALL_IMPORTED_LOCATION "${ProjectRootDir}/ThirdParty/7z2409-extra/x64/7za.dll")
+﻿list(APPEND ALL_IMPORTED_LOCATION "${ProjectRootDir}/ThirdParty/7z2409-extra/x64/7za.dll")
