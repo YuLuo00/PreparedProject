f2658ba23fafaf8c4f0a253054da066f7a05f0a3
yubizhan <yubizhan@qti.qualcomm.com>
2026-06-03 18:41:08 +0800

    [Third party][cmake] one tbb

---
+++ ThirdParty/cmake/Add3rd_tbb.cmake
@@ -0,0 +1,12 @@
+if(MINGW)
+    set(TBB_DIR "${ProjectRootDir}/ThirdParty/tbb/installed/x64-mingw-dynamic/lib/cmake/TBB")
+else()
+    set(TBB_DIR "${ProjectRootDir}/ThirdParty/tbb/installed/x64-windows/share/tbb//")
+endif()
+message("TBB_DIR == ${TBB_DIR}")
+
+find_package(TBB CONFIG REQUIRED)
+target_link_libraries(${PROJECT_NAME} PRIVATE TBB::tbb)
+
+Add_Interface_Imported_Location(TBB::tbb)
+Add_Imported_Location(TBB::tbb)
