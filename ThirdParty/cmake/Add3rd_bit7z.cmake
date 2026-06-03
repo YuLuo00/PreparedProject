f427b9c7de0b674482906db18894a825ff010e4b
yubizhan <yubizhan@qti.qualcomm.com>
2026-06-03 18:45:45 +0800

    [Third party][cmake] bit7z

---
+++ ThirdParty/cmake/Add3rd_bit7z.cmake
@@ -0,0 +1,16 @@
+if(MINGW)
+    set(ghc_filesystem_DIR "${ProjectRootDir}/ThirdParty/bit7z/installed/x64-mingw-dynamic/share/ghc_filesystem/")
+    set(7zip_DIR "${ProjectRootDir}/ThirdParty/bit7z/installed/x64-mingw-dynamic/share/7zip/")
+    set(unofficial-bit7z_DIR "${ProjectRootDir}/ThirdParty/bit7z/installed/x64-mingw-dynamic/share/unofficial-bit7z/")
+else()
+    set(ghc_filesystem_DIR "${ProjectRootDir}/ThirdParty/bit7z/installed/x64-windows/share/ghc_filesystem/")
+    set(7zip_DIR "${ProjectRootDir}/ThirdParty/bit7z/installed/x64-windows/share/7zip/")
+    set(unofficial-bit7z_DIR "${ProjectRootDir}/ThirdParty/bit7z/installed/x64-windows/share/unofficial-bit7z/")
+endif()
+message("unofficial-bit7z_DIR == ${unofficial-bit7z_DIR}")
+
+find_package(unofficial-bit7z CONFIG REQUIRED)
+target_link_libraries(${PROJECT_NAME} PRIVATE unofficial::bit7z::bit7z64)
+
+Add_Interface_Imported_Location(unofficial::bit7z::bit7z64)
+Add_Interface_Imported_Location(7zip::7zip)
